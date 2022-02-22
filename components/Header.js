98506b2903d7062673a5034f5637f91c9728dfdf
Theo Sandell <theo01sandell@gmail.com>
2022-02-22 14:51:54 +0100

    Basic header with text stroke

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,25 @@
+import styles from "../styles/Header.module.css";
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className="h-screen grid grid-rows-3 bg-black">
+        <div className="flex font-bold px-28">
+          <a className={styles.hoverStroke + " text-[200px] text-[#5200FF]"}>
+            Start
+          </a>
+        </div>
+        <div className="flex font-bold px-28">
+          <a className={styles.hoverStroke + " text-[200px] text-[#5200FF]"}>
+            LAB01
+          </a>
+        </div>
+        <div className="flex font-bold px-28">
+          <a className={styles.hoverStroke + " text-[200px] text-[#5200FF]"}>
+            Kontakt
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 }
