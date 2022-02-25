3cef5e9de84aae178ed667f0c92d47bffabba4ac
Theo Sandell <theo01sandell@gmail.com>
2022-02-25 12:52:28 +0100

    Fixed design on end section

--- components/EndSection.js
+++ components/EndSection.js
@@ -3,31 +3,37 @@ import Image from "next/image";
 export default function EndSection() {
   return (
     <div className="pt-[140px]">
-      <div className="">
-        <Image
-          src="https://via.placeholder.com/520x330"
-          width="520px"
-          height="330px"
-        />
-        <Image
-          src="https://via.placeholder.com/450x320"
-          width="450px"
-          height="320px"
-        />
+      <div className="mb-[90px]">
+        <div className="inline-block">
+          <Image
+            src="https://via.placeholder.com/520x330"
+            width="520px"
+            height="330px"
+          />
+        </div>
+        <div className="inline-block translate-x-[50px] translate-y-[50px]">
+          <Image
+            src="https://via.placeholder.com/450x320"
+            width="450px"
+            height="320px"
+          />
+        </div>
       </div>
-      <div>
+      <div className="ml-[90px] flex items-center relative mb-[60px]">
         <Image
           src="https://via.placeholder.com/860x440"
           width="860px"
           height="440px"
         />
-        <Image
-          src="https://via.placeholder.com/160x185"
-          width="160px"
-          height="185px"
-        />
+        <div className="ml-[200px]">
+          <Image
+            src="https://via.placeholder.com/160x185"
+            width="160px"
+            height="185px"
+          />
+        </div>
       </div>
-      <div className="text-white text-[40px]">
+      <div className="text-white text-[40px] w-10/12 mx-auto">
         <p>
           Om Future Experience Lab Future Experience Lab är ett
           innovationsinitiativ för att stärka upplevelsedestinationen Göteborg
