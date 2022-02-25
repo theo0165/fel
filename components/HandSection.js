5da26229138a2fce9a88f8e0592dac87e655e38b
Theo Sandell <theo01sandell@gmail.com>
2022-02-25 13:46:52 +0100

    Fixed "hand" rotation

--- components/HandSection.js
+++ components/HandSection.js
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 export default function HandSection() {
   return (
-    <div className="grid grid-cols-50/50">
+    <div className="grid grid-cols-50/50 text-white h-[400px] overflow-hidden">
       <div className="relative">
-        <Image
-          src="https://via.placeholder.com/750x150"
-          width="750px"
-          height="150px"
-        />
-        <p className="absolute top-0 left-0">
+        <div className="rotate-[6deg] -translate-x-20 absolute -translate-y-1/2 top-1/2">
+          <Image
+            src="https://via.placeholder.com/750x150"
+            width="750px"
+            height="150px"
+          />
+        </div>
+        <p className="absolute -translate-y-1/2 top-1/2 left-0 text-[32px] text-left pl-[90px] pr-[40px]">
           I LAB01: Framtidskreatörerna utforskar vi hur åtta av Göteborgs
           utpräglade destinationsaktörer i samarbete med 34 kreativa
           Yrgo-studerande tillsammans kan skapa framtidens hybridevent. Läs mer
@@ -17,14 +19,21 @@ export default function HandSection() {
         </p>
       </div>
       <div className="relative">
-        <Image
-          src="https://via.placeholder.com/750x150"
-          width="750px"
-          height="150px"
-        />
-        <p className="absolute top-0 left-0">
-          Provkör din verksamhets framtid Behöver du också ett Future Experience
+        <div className="-rotate-[6deg] translate-x-20 absolute -translate-y-1/2 top-1/2">
+          <Image
+            src="https://via.placeholder.com/750x150"
+            width="750px"
+            height="150px"
+          />
+        </div>
+        <p className="absolute -translate-y-1/2 top-1/2 left-0 text-[32px] text-right pr-[90px] pl-[40px]">
+          Provkör din verksamhets framtid.
+          <br />
+          Behöver du också ett Future Experience.
+          <br />
           Lab för din verksamhets framtid?
+          <br />
+          Kontakta oss!
         </p>
       </div>
     </div>
