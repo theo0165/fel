c7a38375e45598d4e15a6bd63f50b17a90cbc83b
Theo Sandell <theo01sandell@gmail.com>
2022-03-02 15:19:10 +0100

    Added intersection observer to "Our labs"

---
+++ components/OurLabs.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { Parallax } from "react-scroll-parallax";
+import { InView } from "react-intersection-observer";
+import { useEffect, useState, useRef } from "react";
+
+export default function OurLabs() {
+  const classes = {
+    inView: "text-blue-500 z-10 text-[200px] text-center font-Wagon font-bold",
+    notInView: "z-10 text-[200px] text-center font-Wagon font-bold",
+  };
+
+  const [elementClasses, setElementClasses] = useState(classes.notInView);
+
+  return (
+    <div className="relative w-full h-[1000px] flex items-center justify-center">
+      <div className="fixed top-0 left-0 bg-blue-200 text-black z-40"></div>
+      <div className="absolute left-0 top-0 -translate-y-[150px] z-10">
+        <Image src="/images/our-labs-circle.png" width="345px" height="850px" />
+      </div>
+      <InView
+        as="div"
+        onChange={(inView, entry) => {
+          setElementClasses(inView ? classes.inView : classes.notInView);
+        }}
+        className="z-10"
+      >
+        <Parallax speed={-10}>
+          <p className={elementClasses}>Our labs</p>
+        </Parallax>
+      </InView>
+      <Image
+        src="/images/eleven.png"
+        layout="fill"
+        className="object-cover object-center"
+      />
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,15 @@
-import Image from "next/image";
-import { Parallax } from "react-scroll-parallax";
 import EndSection from "../components/EndSection";
 import HandSection from "../components/HandSection";
 import Intro from "../components/Intro";
 import LabCollection from "../components/LabCollection";
 import Layout from "../components/Layout";
+import OurLabs from "../components/OurLabs";
 
 export default function Home() {
   return (
     <Layout>
       <Intro />
-      <div className="relative w-full h-[1000px] flex items-center justify-center">
-        <div className="absolute left-0 top-0 -translate-y-[150px] z-10">
-          <Image
-            src="/images/our-labs-circle.png"
-            width="345px"
-            height="850px"
-          />
-        </div>
-        <Parallax speed={-10} className="z-10">
-          <p className="z-10 text-[200px] text-center font-Wagon font-bold">
-            Our labs
-          </p>
-        </Parallax>
-        <Image
-          src="/images/eleven.png"
-          layout="fill"
-          className="object-cover object-center"
-        />
-      </div>
+      <OurLabs />
       <LabCollection />
       <HandSection />
       <EndSection />
