d72e9a51aaff6c97b082daaaad7b1b7a4baf8a34
Theo Sandell <theo01sandell@gmail.com>
2022-03-03 14:16:23 +0100

    Hands updated

--- components/HandSection.js
+++ components/HandSection.js
@@ -6,8 +6,8 @@ export default function HandSection() {
     <div className="grid h-[800px] pt-[100px] grid-cols-50/50 overflow-hidden hand-order:grid-rows-2 hand-order:grid-cols-none hand-order:h-auto bg-main-red text-white">
       <div className="relative hand-order:flex flex-col hand-order:pt-[120px]">
         <div className="rotate-[6deg] -translate-x-[40px] absolute hand-order:static hand-order:-translate-y-0 hand-order:-translate-x-[100px] hand-order:w-fit hand-order:rotate-3 z-40">
-          <Parallax speed={10} translateX={[10, 40]} translateY={[0, 0]}>
-            <Image src="/images/hands/left.png" width="1000px" height="412px" />
+          <Parallax speed={20} translateX={[10, 30]} translateY={[0, 0]}>
+            <Image src="/images/hands/left.png" width="1000px" height="401px" />
           </Parallax>
         </div>
         <p className="absolute -translate-y-1/2 top-[80%] left-0 text-[32px] text-left px-[40px] pb-[100px] hand-order:static hand-order:-translate-y-0 hand-order:pt-[120px]">
@@ -19,11 +19,11 @@ export default function HandSection() {
       </div>
       <div className="relative hand-order:flex flex-col hand-order:py-[60px]">
         <div className="-rotate-[6deg] translate-x-[40px] absolute hand-order:static hand-order:-translate-y-0 hand-order:w-fit hand-order:self-end hand-order:-rotate-3 hand-order:translate-x-[100px]">
-          <Parallax speed={10} translateX={[-10, -40]} translateY={[0, 0]}>
+          <Parallax speed={20} translateX={[-10, -30]} translateY={[0, 0]}>
             <Image
               src="/images/hands/right.png"
               width="1000px"
-              height="412px"
+              height="395px"
             />
           </Parallax>
         </div>
--- components/Header.js
+++ components/Header.js
@@ -3,22 +3,27 @@ import styles from "../styles/Header.module.css";
 export default function Header() {
   return (
     <header className="font-['Wagon']">
-      {/*
-      <div className="topBar w-11/12 border-b-2 border-white flex justify-between py-3 mx-auto text-white absolute top-0 right-[50%] translate-x-1/2 z-20">
-        <div>LOGO</div>
-        <div>COLORS</div>
+      <div className="topBar w-full py-3 mx-auto text-[#DBDBDB] absolute top-0 right-[50%] translate-x-1/2 z-20">
+        <div className="font-bold tex-[50px] text-right pr-[20px]">
+          <p>
+            Future.
+            <br />
+            Experience.
+            <br />
+            Lab.
+          </p>
+        </div>
       </div>
-      */}
-      <div className="h-screen grid grid-rows-3 bg-black text-[150px] header-break:text-[100px] header-smallest-text:text-[50px]">
-        <div className="relative header-break:flex header-break:items-center">
+      <div className="h-screen grid grid-rows-3 bg-black text-[175px] header-break:text-[100px] header-smallest-text:text-[50px]">
+        <div className="relative flex items-end">
           <a
             href="#"
-            className=" text-main-red z-30 pl-28 relative uppercase font-bold hover:font-extralight hover:italic header-smallest-text:pl-[20px]"
+            className=" text-main-red z-30 pl-[40px] relative uppercase font-bold hover:font-extralight hover:italic header-smallest-text:pl-[20px]"
             tabIndex="2"
           >
             labs
           </a>
-          <a className="text-blue-green font-bold pl-[100px] relative z-30 header-break:pl-[50px] header-smallest-text:pl-[20px]">
+          <a className="text-blue-green font-bold pl-[40px] relative z-30 header-break:pl-[50px] header-smallest-text:pl-[20px]">
             01 02
           </a>
           <video
@@ -29,10 +34,10 @@ export default function Header() {
             className="absolute right-0 top-0 w-screen h-full object-cover"
           />
         </div>
-        <div className="font-bold relative header-break:flex header-break:items-center">
+        <div className="font-bold relative flex items-center">
           <a
             href="#"
-            className="text-main-red z-30 pl-28 relative uppercase hover:font-extralight hover:italic header-smallest-text:pl-[20px]"
+            className="text-main-red z-30 pl-[40px] relative uppercase hover:font-extralight hover:italic header-smallest-text:pl-[20px]"
             tabIndex="3"
           >
             Future
@@ -45,10 +50,10 @@ export default function Header() {
             className="absolute right-0 top-0 w-screen h-full object-cover"
           />
         </div>
-        <div className="font-bold relative header-break:flex header-break:items-center">
+        <div className="font-bold relative header-break:flex header-break:items-start header-break:pt-[20px]">
           <a
             href="#"
-            className="text-main-red z-30 pl-28 relative uppercase hover:font-extralight hover:italic header-smallest-text:pl-[20px]"
+            className="text-main-red z-30 pl-[40px] relative uppercase hover:font-extralight hover:italic header-smallest-text:pl-[20px]"
             tabIndex="3"
           >
             Contact
